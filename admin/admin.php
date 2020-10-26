fbb30b8f3662bdb0b05c62a7849557b86874d3a4
clausH1994 <44051013+clausH1994@users.noreply.github.com>
2020-10-26 10:18:11 +0100

    functions

---
+++ admin/admin.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body class="center">
+
+    <div>
+    <h2>Please login</h2>
+    <form action="" method="post">
+        Username:
+        <input type="text" name="user" maxlength="30" value="" />
+        Password:
+        <input type="password" name="pass" maxlength="30" value="" />
+        <input type="submit" name="submit" value="Login" />
+    </form>
+    </div>
+</body>
+</html>
